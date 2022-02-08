99fea83dd201a98927abb4c5d9639ba98cf3a1a9
Guangxu Li <guangxu.li@nyu.edu>
2022-02-09 00:20:27 +0800

    fix: correctly use global variable

--- main.py
+++ main.py
@@ -5,18 +5,19 @@ import discord
 from dexscreener import DexscreenerClient
 
 BOT_TOKEN = 'OTM5OTI1OTUzNjQ2MzA5NDE4.Yf_8Qg.KAL9wScBMaMbVqcFotUcxEHpJk0'
-INTERVAL = 5
+INTERVAL = 1
 
 discord_client = discord.Client()
 dex_client = DexscreenerClient()
 
-prev_price = 0.0
+prev_price = float(0.0)
 
 async def push_update():
     history = dex_client.recent_trade_history("fantom", "0x937813380c7b98a66afa5992bf2231e3e5913ef4")
     most_recent = history.trade_history[0]
     price = most_recent.price_usd
 
+    global prev_price
     if price == prev_price:
         return
     prev_price = price
@@ -32,7 +33,6 @@ async def on_ready():
             await push_update()
         except:
             continue
-        
 
 if __name__ == "__main__":
     discord_client.run(BOT_TOKEN)
