cc9fbb27441e929f91144ac3000e248079387f78
Guangxu Li <guangxu.li@nyu.edu>
2022-02-08 22:48:36 +0800

    refactor: avoid API call in on_ready

--- main.py
+++ main.py
@@ -10,16 +10,18 @@ INTERVAL = 5
 discord_client = discord.Client()
 dex_client = DexscreenerClient()
 
+async def push_update():
+    for guild in discord_client.guilds:
+        history = dex_client.recent_trade_history("fantom", "0x937813380c7b98a66afa5992bf2231e3e5913ef4")
+        most_recent = history.trade_history[0]
+        price = most_recent.price_usd
+
+    await guild.me.edit(nick=f"Rich @ USD%.2f"%price)
+
 @discord_client.event
 async def on_ready():
     while True:
-        for guild in discord_client.guilds:
-            history = dex_client.recent_trade_history("fantom", "0x937813380c7b98a66afa5992bf2231e3e5913ef4")
-            most_recent = history.trade_history[0]
-            price = most_recent.price_usd
-
-            await guild.me.edit(nick=f"Rich @ USD%.2f"%price)
-
+        await push_update()
         await sleep(INTERVAL)
 
 if __name__ == "__main__":
